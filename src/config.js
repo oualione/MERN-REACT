edc6a4c031ca151cc8245efe40a85b19bfe1fe12
Khalid Ouali <ouali.linux@gmail.com>
2024-05-15 16:38:15 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://localhost:7000/api';
-export const API_URL = 'https://e-commerce-backend-tan-ten.vercel.app/';
+export const API_URL = 'https://e-commerce-backend-tan-ten.vercel.app';
