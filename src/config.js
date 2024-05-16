63e9e1e1f88f9964598807d9e63622c0c82dd90e
Khalid Ouali <ouali.linux@gmail.com>
2024-05-16 17:47:37 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // export const API_URL = 'http://localhost:7000/api';
-export const API_URL = 'https://e-commerce-backend-six-opal.vercel.app/api';
+export const API_URL = 'https://e-commerce-backend-se8r.onrender.com/api';
 
